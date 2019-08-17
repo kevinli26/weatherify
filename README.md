c2da995f02fadbac8c93c748ecfa1c89605e5b09
Kevin Li <kevinli1226@gmail.com>
2019-08-17 18:12:02 -0400

    Updated README and converted all api calls to Axios

--- README.md
+++ README.md
@@ -8,14 +8,12 @@ Client (consists of two primary views)
 
 Server
   * All data retreival, parsing, validation, is done in the backend via express routes and application-level middleware.
-  * Essentially serves as a beacon that responds to RESTful requests from the client (which are then handled by a series of
-  Expres middleware functions)
+  * Essentially serves as a beacon that responds to RESTful requests from the client (which are then handled by a series of Express middleware functions)
  
 # To-do
-  - [ ] Implement dynamic resizing for client mountable views with React-vis.
+  - [X] Implement dynamic resizing for client mountable views with Formidable Victory.
   - [ ] Integrate websockets with socket.io.
   - [X] Allow client to search a new location with client-side hydration.
-  - [ ] Transform pure SSR into an isomorphic application.
   - [ ] Replace .ejs views with react views.
 
 
--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+//import required node modules
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
@@ -5,103 +6,186 @@ const request = require('request');
 const port = 4000;
 const fnc = require('./routes/backendFunctions.js'); //include backendFunctions
 const moment = require('moment');
+const axios = require('axios'); 
 
+//set up required express middeware
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json())
+//set up template engines
 app.set('views', __dirname + '/views');
 app.set('view engine', 'jsx');
 app.set('view engine', 'ejs');
 app.engine('jsx', require('express-react-views').createEngine());
 app.use(express.static(__dirname + '/public'));
 
-app.get('/ping', (req,res) => {
-	console.log('pong');
-	res.end();
-})
-
 //TO-DO: modularize the different functions, organize it better, put into different routes then import these routes, use as many helper functions as possible, still want to stick with server-side rendering
+// app.post('/weather', (req,res) => {
+// 	//opecagedata api to fetch long and lat from address via geocoding
+// 	const geoKey = "bc01996fe2d64f76a01fa21e9eeb63a3";
+// 	let location = req.body.location;
+// 	location = encodeURIComponent(location);
+// 	let geourl ="https://api.opencagedata.com/geocode/v1/json?q="+location+"&key="+geoKey; 
+// 	let latitude = "";
+// 	let longitude = "";
+// 	request(geourl, function (err, response, body) {
+// 		if(err){
+// 	    	console.log('error:', err);
+// 			res.render('error.jsx', { errormessage: "Unable to get the geocoding data."});
+// 	    } else{
+// 			let resultLocation = JSON.parse(body)
+			
+// 	    	//no results hence invalid address
+// 	    	if (resultLocation.total_results == 0) {
+// 	    		console.log('error:', err);
+// 				res.render('error.jsx', { errormessage: "invalid address"});
+// 	    	}
+
+// 	    	else {
+// 	    		console.log("valid address");
+// 		    	let bounds = ((resultLocation.results)[0]).geometry;
+// 		    	latitude = bounds.lat;
+// 		    	longitude = bounds.lng;
+// 		    	//call darksky api to get weather data based on the long and lat
+// 				let apiKey = '987b2fb820686e246e617bc308c3d206';
+// 				let url = 'https://api.darksky.net/forecast/'+apiKey+'/'+latitude+','+longitude+'?exclude=minutely,flags';
+
+// 				request(url, function (err, response, body) {
+// 					if(err){
+// 				    	console.log('error:', error);
+// 						res.render('error.jsx', { errormessage: "Unable to get the weather data."});
+						
+// 				    } else{
+// 						console.log("serving weather info");
+// 						let weather = JSON.parse(body); //parse to json so we can handle it
+						
+// 						// // print everything for debugging and info purposes
+// 						// var parsed = JSON.stringify(weather, null, 2);
+// 						// console.log(parsed);
+						
+// 						//Right now
+// 						let current = weather.currently;
+// 						let currentTime = moment.unix(current.time).format("LLL");//current time in context of the timezone of the user
+// 						let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+// 						let timeZoneLocation = weather.timezone; //timezone of the searched location (disregarding the users location)	
+
+// 						//today
+// 						let day = weather.hourly.data;
+// 						let daySummary = weather.hourly.summary;
+// 						day = fnc.parseToday(day);
+						
+// 						//Week
+// 						let week = weather.daily.data;
+// 						let weekSummary = weather.daily.summary;
+// 						weekSummary = fnc.parseWeekSummaryMessage(weekSummary)
+// 						week = fnc.parseWeek(week);
+												
+// 						const data = {
+// 							//right now
+// 							location: decodeURIComponent(location), //user entered location
+// 							time: currentTime,
+// 							rightNow: current,
+// 							//for the day
+// 							day: day, //data for daily weather (data as parsed due to requirements)
+// 							daySummary: daySummary,
+// 							//for the week
+// 							weekSummary: weekSummary,
+// 							week: week,
+// 							//general user search information
+// 							timeZone: timeZone,
+// 							timeZoneLocation: timeZoneLocation,
+// 						}
+
+// 						res.render('App.jsx', data );
+// 				  	}
+// 				});
+// 	    	}
+// 	  	}
+// 	});
+// });
+
 app.post('/weather', (req,res) => {
+	const geoKey = "bc01996fe2d64f76a01fa21e9eeb63a3"; //To-Do: hide this safely when deploying
 	//opecagedata api to fetch long and lat from address via geocoding
-	const geoKey = "bc01996fe2d64f76a01fa21e9eeb63a3";
 	let location = req.body.location;
 	location = encodeURIComponent(location);
 	let geourl ="https://api.opencagedata.com/geocode/v1/json?q="+location+"&key="+geoKey; 
-	let latitude = "";
-	let longitude = "";
-	request(geourl, function (err, response, body) {
-		if(err){
-	    	console.log('error:', err);
-			res.render('error.jsx', { errormessage: "Unable to get the geocoding data."});
-	    } else{
-			let resultLocation = JSON.parse(body)
-			
-	    	//no results hence invalid address
-	    	if (resultLocation.total_results == 0) {
-	    		console.log('error:', err);
-				res.render('error.jsx', { errormessage: "invalid address"});
-	    	}
+	let latitude, longitude;
+	axios({
+		method: 'get',
+		url: geourl,
+	}).then( response => {
+		let resultLocation = response.data; //response from API
 
-	    	else {
-	    		console.log("valid address");
-		    	let bounds = ((resultLocation.results)[0]).geometry;
-		    	latitude = bounds.lat;
-		    	longitude = bounds.lng;
-		    	//call darksky api to get weather data based on the long and lat
-				let apiKey = '987b2fb820686e246e617bc308c3d206';
-				let url = 'https://api.darksky.net/forecast/'+apiKey+'/'+latitude+','+longitude+'?exclude=minutely,flags';
+		//no results hence invalid address
+		if (resultLocation.total_results == 0) {
+			console.log('Invalid address, error:', err);
+			res.render('error.jsx', { errormessage: "invalid address"});
+		}
 
-				request(url, function (err, response, body) {
-					if(err){
-				    	console.log('error:', error);
-						res.render('error.jsx', { errormessage: "Unable to get the weather data."});
-						
-				    } else{
-						console.log("serving weather info");
-						let weather = JSON.parse(body); //parse to json so we can handle it
-						
-						// // print everything for debugging and info purposes
-						// var parsed = JSON.stringify(weather, null, 2);
-						// console.log(parsed);
-						
-						//Right now
-						let current = weather.currently;
-						let currentTime = moment.unix(current.time).format("LLL");//current time in context of the timezone of the user
-						let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
-						let timeZoneLocation = weather.timezone; //timezone of the searched location (disregarding the users location)	
+		else {
+			console.log("Valid address");
+			let bounds = ((resultLocation.results)[0]).geometry;
+			latitude = bounds.lat;
+			longitude = bounds.lng;
+			//call darksky api to get weather data based on the long and lat
+			let apiKey = '987b2fb820686e246e617bc308c3d206';
+			let weatherurl = 'https://api.darksky.net/forecast/'+apiKey+'/'+latitude+','+longitude+'?exclude=minutely,flags';
 
-						//today
-						let day = weather.hourly.data;
-						let daySummary = weather.hourly.summary;
-						day = fnc.parseToday(day);
-						
-						//Week
-						let week = weather.daily.data;
-						let weekSummary = weather.daily.summary;
-						weekSummary = fnc.parseWeekSummaryMessage(weekSummary)
-						week = fnc.parseWeek(week);
-												
-						const data = {
-							//right now
-							location: decodeURIComponent(location), //user entered location
-							time: currentTime,
-							rightNow: current,
-							//for the day
-							day: day, //data for daily weather (data as parsed due to requirements)
-							daySummary: daySummary,
-							//for the week
-							weekSummary: weekSummary,
-							week: week,
-							//general user search information
-							timeZone: timeZone,
-							timeZoneLocation: timeZoneLocation,
-						}
+			axios({
+				method: 'get',
+				url: weatherurl,
+			}).then( response => {
+				console.log("Got weather data, serving data now");
+				console.log("serving weather info");
+				let weather = response.data; //parse to json so we can handle it
+				
+				// // print everything for debugging and info purposes
+				// var parsed = JSON.stringify(weather, null, 2);
+				// console.log(parsed);
+				
+				//Right now
+				let current = weather.currently;
+				let currentTime = moment.unix(current.time).format("LLL");//current time in context of the timezone of the user
+				let timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+				let timeZoneLocation = weather.timezone; //timezone of the searched location (disregarding the users location)	
 
-						res.render('App.jsx', data );
-				  	}
-				});
-	    	}
-	  	}
-	});
+				//today
+				let day = weather.hourly.data;
+				let daySummary = weather.hourly.summary;
+				day = fnc.parseToday(day);
+				
+				//Week
+				let week = weather.daily.data;
+				let weekSummary = weather.daily.summary;
+				weekSummary = fnc.parseWeekSummaryMessage(weekSummary)
+				week = fnc.parseWeek(week);
+										
+				const data = {
+					//right now
+					location: decodeURIComponent(location), //user entered location
+					time: currentTime,
+					rightNow: current,
+					//for the day
+					day: day, //data for daily weather (data as parsed due to requirements)
+					daySummary: daySummary,
+					//for the week
+					weekSummary: weekSummary,
+					week: week,
+					//general user search information
+					timeZone: timeZone,
+					timeZoneLocation: timeZoneLocation,
+				}
+
+				res.render('App.jsx', data );
+			}).catch( err => {
+				console.log('Unable to get weather data, error:', err);
+				res.render('error.jsx', { errormessage: "Unable to get the weather data."});
+			})
+		}
+	}).catch( err => {
+		console.log('error:', err);
+		res.render('error.jsx', { errormessage: "Unable to get the geocoding data."});
+	})
 });
 
 app.get('/', (req, res) => {
